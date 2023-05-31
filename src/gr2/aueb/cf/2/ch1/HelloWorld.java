05f9fdf6c8aac5b3204d7ea524f39b30eccdcf77
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-05-31 17:50:15 +0300

    Update HelloWorld.v0.1.java

--- src/gr2/aueb/cf/2/ch1/HelloWorld.java
+++ src/gr2/aueb/cf/2/ch1/HelloWorld.java
@@ -1,20 +1,14 @@
 package gr2.aueb.cf;
 
 /**
- * Εκτυπωνει Hello World! στο
- * standard output (κονσολα).
- *
- * @author a8ana
+ * Prints Hello World! in the
+ * standard output (console).
  */
-
 public class HelloWorld {
     
     public static void main(String[] args) {
 
         // Prints Hello World!
-        /*
-
-        */
 
         System.out.println("Hello World!");
     }
