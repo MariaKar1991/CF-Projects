63038ccd8fe93709e7eec1af35aad0ca0fa2afad
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-05-31 18:20:10 +0300

    AverageApp.v0.1.java

--- src/gr2/aueb/cf/2/ch3/AverageApp.java
+++ src/gr2/aueb/cf/2/ch3/AverageApp.java
@@ -3,8 +3,8 @@ package gr2.aueb.cf;
 import java.util.Scanner;
 
 /**
- *  Βρισκει τον μεσο ορο της βαθμολογιας ενος
- *  μαθητη. Παρεχονται τα total και count.
+ * Finds the average score of a
+ * student. Total and count are provided.
  */
 public class AverageApp {
     public static void main(String[] args) {
