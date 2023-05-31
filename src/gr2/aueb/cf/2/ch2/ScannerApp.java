79cb8f0ce8ff1d4a4d88fe4dece46d2510ad7bd8
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-05-31 18:11:45 +0300

    Update ScannerApp.v0.1.java

--- src/gr2/aueb/cf/2/ch2/ScannerApp.java
+++ src/gr2/aueb/cf/2/ch2/ScannerApp.java
@@ -3,27 +3,23 @@ package gr2.aueb.cf;
 import java.util.Scanner;
 
 /**
- * Διαβαζει απο το std input(πληκτρολογιο)
- * δυο ακεραιους και τους προσθετει.
+ * Reads from std input (keyboard)
+ * two integers and adds them.
  */
 public class ScannerApp {
 
     public static void main(String[] args) {
-
-        // Δηλωση και αρχικοποιηση
+        
         Scanner in = new Scanner(System.in);
         int num1 = 0;
         int num2 = 0;
         int sum = 0;
-
-        // Εντολες
 
         System.out.println("Please insert two ints");
         num1 = in.nextInt();
         num2 = in.nextInt();
         sum = num1 + num2;
 
-        //Εκτυπωση αποτελεσματων
         System.out.printf("%d + %d = %d", num1, num2, sum);
         num1 = in.nextInt();
     }
